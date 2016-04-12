93019e7c06592f36c1d6c33d99cdb95b1e5d8d0d
Stephane Epardaud <stef@epardaud.fr>
2016-04-12 17:44:38 +0200

    Fair icons for sponsors

--- app/models/Sponsor.java
+++ app/models/Sponsor.java
@@ -1,5 +1,9 @@
 package models;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -7,10 +11,14 @@ import javax.persistence.Lob;
 
 import org.hibernate.annotations.Type;
 
+import play.Logger;
+import play.data.binding.ParamNode;
 import play.data.validation.MaxSize;
 import play.data.validation.Required;
 import play.data.validation.URL;
 import play.db.jpa.Blob;
+import play.db.jpa.GenericModel;
+import play.db.jpa.JPABase;
 import play.db.jpa.Model;
 
 @Entity
@@ -31,6 +39,27 @@ public class Sponsor extends Model implements Comparable<Sponsor> {
 	
 	public Blob logo;
 	
+	// set on save/create
+	public Integer width, height;
+	
+	@Override
+	public void _save() {
+		updateImageSizes();
+		super._save();
+	}
+	
+	private void updateImageSizes() {
+		if(logo != null){
+			try {
+				BufferedImage image = ImageIO.read(logo.getFile());
+				this.width = image.getWidth();
+				this.height = image.getHeight();
+			} catch (IOException e) {
+				Logger.error(e, "Failed to read image");
+			}
+		}
+	}
+
 	@Override
 	public String toString() {
 		return company;
@@ -42,5 +71,25 @@ public class Sponsor extends Model implements Comparable<Sponsor> {
 		if(ret != 0)
 			return ret;
 		return company.compareTo(other.company);
+	}
+	
+	public int getWidth(int surface, int maxSide){
+		if(width == null)
+			return 0;
+		int biggerSide = Math.max(width, height);
+		float sizeFactor = (float)maxSide / biggerSide;
+		float area = width * height * sizeFactor * sizeFactor;
+		float areaFactor = Math.min(1, (float)surface / area);
+		return (int)Math.floor(width * sizeFactor * areaFactor);
+	}
+
+	public int getHeight(int surface, int maxSide){
+		if(height == null)
+			return 0;
+		int biggerSide = Math.max(width, height);
+		float sizeFactor = (float)maxSide / biggerSide;
+		float area = width * height * sizeFactor * sizeFactor;
+		float areaFactor = Math.min(1, (float)surface / area);
+		return (int)Math.floor(height * sizeFactor * areaFactor);
 	}
 }
