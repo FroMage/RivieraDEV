2b21d1dd4f297035c67c938eedfe919670be6616
Michèle <michele@MacBook-Pro-de-Michele.local>
2016-10-28 21:10:26 +0200

    Styles Title and countdown

--- public/javascripts/countdown.js
+++ public/javascripts/countdown.js
@@ -1,9 +1,22 @@
 $(function() {
     var finalDate = '2017/05/11 08:30:00';
-    var format = '<span class="countdown-day">%D</span>'
-    + '<span class="countdown-hour">%H</span>'
-    + '<span class="countdown-min">%M</span>'
-    + '<span class="countdown-sec">%S</span>';
+    var format = 
+      '<span class="countdown-day">'
+    + '    <span class="countdown-number">%D</span>'
+    + '    <span class="countdown-desc">Jours</span>'
+    + '</span>'
+    + '<span class="countdown-hour">'
+    + '    <span class="countdown-number">%H</span>'
+    + '    <span class="countdown-desc">Heures</span>'
+    + '</span>'
+    + '<span class="countdown-min">'
+    + '    <span class="countdown-number">%M</span>'
+    + '    <span class="countdown-desc">Minutes</span>'
+    + '</span>'
+    + '<span class="countdown-sec">'
+    + '    <span class="countdown-number">%S</span>'
+    + '    <span class="countdown-desc">Secondes</span>'
+    + '</span>';
 
     $('#js-countdown').countdown(finalDate, function(event) {
         $(this).html(event.strftime(format));
