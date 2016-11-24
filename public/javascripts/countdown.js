c271911b320d7768064036e666b00db52825cee3
Stephane Epardaud <stef@epardaud.fr>
2016-11-24 13:34:42 +0100

    Traductions

--- public/javascripts/countdown.js
+++ public/javascripts/countdown.js
@@ -3,19 +3,19 @@ $(function() {
     var format = 
       '<span class="countdown-day">'
     + '    <span class="countdown-number">%D</span>'
-    + '    <span class="countdown-desc">Jours</span>'
+    + '    <span class="countdown-desc">' + countDownDaysLabel + '</span>'
     + '</span>'
     + '<span class="countdown-hour">'
     + '    <span class="countdown-number">%H</span>'
-    + '    <span class="countdown-desc">Heures</span>'
+    + '    <span class="countdown-desc">' + countDownHoursLabel + '</span>'
     + '</span>'
     + '<span class="countdown-min">'
     + '    <span class="countdown-number">%M</span>'
-    + '    <span class="countdown-desc">Minutes</span>'
+    + '    <span class="countdown-desc">' + countDownMinutesLabel + '</span>'
     + '</span>'
     + '<span class="countdown-sec">'
     + '    <span class="countdown-number">%S</span>'
-    + '    <span class="countdown-desc">Secondes</span>'
+    + '    <span class="countdown-desc">' + countDownSecondsLabel + '</span>'
     + '</span>';
 
     $('#js-countdown').countdown(finalDate, function(event) {
