51c7b7fb6ddf4f7c0bba4ed9830e1303d117da38
Jean-Michael <mika@iMac-de-Michele.local>
2017-02-25 17:04:07 +0100

    Filter on level

--- public/javascripts/schedule-filter.js
+++ public/javascripts/schedule-filter.js
@@ -1,65 +1,59 @@
 $(function() {
 
     window.toggleTheme = function (theme){
-        toggleThemeFilterClass($('.fullSchedule-talk-theme-' + theme));
+        //toggleThemeFilterClass($('.fullSchedule-talk-theme-' + theme));
+		filterSchedule('theme',theme, toggleThemeFilterClass)
 	}
 
-    window.toggleTheme2 = function (theme){
+    window.toggleLevel = function (level){
+        //toggleLevelFilterClass($('.fullSchedule-talk-level-' + level));
+		filterSchedule('level',level, toggleLevelFilterClass)
+    }
+
+    window.filterSchedule = function (filter, option, filterMethod){
 		// Le filtre sélectionné
-		let currentFilter = $('.fullSchedule-filter-themes .fullSchedule-talk-theme-' + theme)
+		let currentFilter = $('.fullSchedule-filter-' + filter + 's .fullSchedule-talk-' + filter + '-' + option)
 		console.log('Filtre courant:',currentFilter)
 
 		// Les éléments à filtrer
-		let currentElems = $('.fullSchedule-talk-theme-' + theme)
+		let currentElems = $('.fullSchedule-talk-' + filter + '-' + option)
 		console.log('Elms:',currentElems)
 
-		if(currentFilter && currentFilter.attr('data-filter-theme')){
+		if(currentFilter && currentFilter.attr('data-filter-' + filter)){
 			// Si element courant est désactivé alors le réactiver
-			console.log(theme + " doit être visible");
+			console.log(option + " doit être visible");
 			currentElems.each(function(index, element) {
-				toggleThemeFilterClass(element);
+				filterMethod(element);
 			}, this);
 		}
 		else{
-			console.log(theme + " doit être caché");
+			console.log(option + " doit être caché");
 
-			let otherHiddenFilters = $('.fullSchedule-filter-themes .fullSchedule-filter-item[data-filter-theme]').not('.fullSchedule-talk-theme-' + theme)
-			let otherFilters = $('.fullSchedule-filter-themes .fullSchedule-filter-item').not('.fullSchedule-talk-theme-' + theme)
+			let otherFilters = $('.fullSchedule-filter-' + filter + 's .fullSchedule-filter-item').not('.fullSchedule-talk-' + filter + '-' + option)
+			let otherHiddenFilters = otherFilters.not('[data-filter-' + filter + ']')
 			// Si d'autres éléments sont déjà cachés alors on cache l'élément sélectionné
 			if(otherHiddenFilters.length > 0 && otherHiddenFilters.length !== otherFilters.length){
 				currentElems.each(function(index, element) {
-					toggleThemeFilterClass(element);
+					filterMethod(element);
 				}, this);
 				return;
 			}
 
 			// Si aucun éléments n'est cachés alors on cache tous les autres
-
-			let otherElems = $('.fullSchedule-talk .fullSchedule-filter-item').not('.fullSchedule-talk-theme-' + theme)
+			let otherElems = $('.fullSchedule-talk .fullSchedule-filter-item').not('.fullSchedule-talk-' + filter + '-' + option)
 			if(otherFilters.length > 0){
 				otherFilters.each(function(index, element) {
-					toggleThemeFilterClass(element);
+					filterMethod(element);
 				}, this);
 				otherElems.each(function(index, element) {
-					toggleThemeFilterClass(element);
+					filterMethod(element);
 				}, this);
 			}
 			else{
-				toggleThemeFilterClass($('.fullSchedule-talk-theme-' + theme));
+				filterMethod($('.fullSchedule-talk-' + filter + '-' + option));
 			}
 			console.log('Elms:',currentElems)
 		}
-
-
-		// Sinon {
-			// Si aucun élément désactivé on désactive tous les autres
-			// Sinon on désactive l'élément courant
-		//}
-
-    }
-
-    window.toggleLevel = function (level){
-        toggleLevelFilterClass($('.fullSchedule-talk-level-' + level));
     }
 
     var toggleThemeFilterClass = function (elem){
@@ -70,7 +64,7 @@ $(function() {
 		toggleFilterClass(elem, 'data-filter-level', 'hidden');
 	}
 	
-    var toggleFilterClass = function(elem, attrName, className){
+    var toggleFilterClassOLD = function(elem, attrName, className){
         if(elem && (!elem.attr(attrName))){
             elem.attr(attrName,className);
 		}
@@ -79,7 +73,7 @@ $(function() {
         }
     }
 
-	var toggleFilterClass2 = function(elem, attrName, className){
+	var toggleFilterClass = function(elem, attrName, className){
 		if(elem && (!elem.getAttribute(attrName))){
 			elem.setAttribute(attrName,className);
 		}
