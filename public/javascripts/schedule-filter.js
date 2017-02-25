0677173bcff8e98533ffe29c310b2095b4fb4106
Jean-Michael <mika@iMac-de-Michele.local>
2017-02-25 17:13:20 +0100

    Clean code of filter

--- public/javascripts/schedule-filter.js
+++ public/javascripts/schedule-filter.js
@@ -1,34 +1,27 @@
 $(function() {
 
     window.toggleTheme = function (theme){
-        //toggleThemeFilterClass($('.fullSchedule-talk-theme-' + theme));
 		filterSchedule('theme',theme, toggleThemeFilterClass)
 	}
 
     window.toggleLevel = function (level){
-        //toggleLevelFilterClass($('.fullSchedule-talk-level-' + level));
 		filterSchedule('level',level, toggleLevelFilterClass)
     }
 
     window.filterSchedule = function (filter, option, filterMethod){
 		// Le filtre sélectionné
 		let currentFilter = $('.fullSchedule-filter-' + filter + 's .fullSchedule-talk-' + filter + '-' + option)
-		console.log('Filtre courant:',currentFilter)
 
 		// Les éléments à filtrer
 		let currentElems = $('.fullSchedule-talk-' + filter + '-' + option)
-		console.log('Elms:',currentElems)
 
 		if(currentFilter && currentFilter.attr('data-filter-' + filter)){
 			// Si element courant est désactivé alors le réactiver
-			console.log(option + " doit être visible");
 			currentElems.each(function(index, element) {
 				filterMethod(element);
 			}, this);
 		}
 		else{
-			console.log(option + " doit être caché");
-
 			let otherFilters = $('.fullSchedule-filter-' + filter + 's .fullSchedule-filter-item').not('.fullSchedule-talk-' + filter + '-' + option)
 			let otherHiddenFilters = otherFilters.not('[data-filter-' + filter + ']')
 			// Si d'autres éléments sont déjà cachés alors on cache l'élément sélectionné
@@ -52,7 +45,6 @@ $(function() {
 			else{
 				filterMethod($('.fullSchedule-talk-' + filter + '-' + option));
 			}
-			console.log('Elms:',currentElems)
 		}
     }
 
@@ -63,15 +55,6 @@ $(function() {
 	var toggleLevelFilterClass = function (elem){
 		toggleFilterClass(elem, 'data-filter-level', 'hidden');
 	}
-	
-    var toggleFilterClassOLD = function(elem, attrName, className){
-        if(elem && (!elem.attr(attrName))){
-            elem.attr(attrName,className);
-		}
-        else{
-            elem.removeAttr(attrName,className);
-        }
-    }
 
 	var toggleFilterClass = function(elem, attrName, className){
 		if(elem && (!elem.getAttribute(attrName))){
