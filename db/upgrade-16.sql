d9fe141c8e906a95c0fc679f22188f4d592188ab
Stephane Epardaud <stef@epardaud.fr>
2019-03-04 15:57:59 +0100

    New DB

---
+++ db/upgrade-16.sql
@@ -0,0 +1,5 @@
+alter table speaker add column importId varchar(255);
+alter table speaker add column phone varchar(255);
+alter table talk add column importId varchar(255);
+alter table TalkTheme add column importId varchar(255);
+alter table TalkType add column importId varchar(255);
