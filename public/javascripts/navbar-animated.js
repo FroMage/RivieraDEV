3ba1095484386fcccf9ad2f11187ad4f51a2063a
Jean-Michael <jeanmichael.legait@gmail.com>
2019-03-11 21:53:53 +0100

    Style for Mobile Menu

--- public/javascripts/navbar-animated.js
+++ public/javascripts/navbar-animated.js
@@ -1,38 +1,55 @@
-$(function () {
-    var navbar = $(".js-navbar-animated");
+$(function() {
+    // Mobile Menu -------
+    window.initBurgerMenu = () => {
+        var burgerMenu = $('.js-burgerMenu-input');
+        let burgerMenuOpenClass = 'burgerMenu--open';
 
-    var toggleAnimatedClass = function(){
-         var windowWidth = $(window).width(),
-             screenXsMax = 767;
+        if (burgerMenu && burgerMenu.length > 0)
+            burgerMenu[0].addEventListener('change', event => {
+                let body = document.getElementsByTagName('body')[0];
+                if (event.target.checked) {
+                    body.classList.add(burgerMenuOpenClass);
+                } else {
+                    body.classList.remove(burgerMenuOpenClass);
+                }
+            });
+    };
 
-         if(windowWidth <= screenXsMax){
-            navbar.removeClass("navbar-animated");
-         }
-         else{
-            navbar.addClass("navbar-animated");
-         }
+    // Desktop Menu ------
+    var navbar = $('.js-navbar-animated');
+
+    var toggleAnimatedClass = function() {
+        var windowWidth = $(window).width(),
+            screenXsMax = 767;
+
+        if (windowWidth <= screenXsMax) {
+            navbar.removeClass('navbar-animated');
+        } else {
+            navbar.addClass('navbar-animated');
+        }
     };
 
     var resizeNavbar = function() {
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+        var distanceY =
+                window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 300;
 
         if (distanceY > shrinkOn) {
-            navbar.addClass("navbar-animated-small");
+            navbar.addClass('navbar-animated-small');
         } else {
-            if (navbar.hasClass("navbar-animated-small")) {
-                navbar.removeClass("navbar-animated-small");
+            if (navbar.hasClass('navbar-animated-small')) {
+                navbar.removeClass('navbar-animated-small');
             }
         }
     };
 
-    window.addEventListener('scroll', function(e){
+    window.addEventListener('scroll', function(e) {
         resizeNavbar();
     });
 
-     window.addEventListener('resize', function(e){
+    window.addEventListener('resize', function(e) {
         toggleAnimatedClass();
-     });
+    });
 
     toggleAnimatedClass();
     resizeNavbar();
