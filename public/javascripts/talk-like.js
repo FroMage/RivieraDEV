251eac8e7107727a72e23a6e7b4f5080e2b8a8e1
Michèle Legait <michele@travelaer.com>
2019-04-01 23:20:17 +0200

    Fix Like Talk

--- public/javascripts/talk-like.js
+++ public/javascripts/talk-like.js
@@ -1,107 +1,106 @@
 $(function() {
-    window.likeTalk = function (elem, id){
-		let $elem = $(elem);
-		let like = $elem.attr('data-like');
-		if(like) {
-			// The user doesn't want to like this talk anymore
-			_unlikeIcon($elem);
-			_removeLike(id);
-		}
-		else {
-			// The user likes the talk
-			_likeIcon($elem);
-			_addLike(id);
-		}
-	}
-		
-	// Initialize Liked Talks
-	window.initLikedTalks = function () {
-		let likes = _getLikes();
-		for (var i = 0; i < likes.length; i++) {
-			_likeIcon($('[data-talk=' + likes[i] + ']'))
-		}
-	}
+    window.likeTalk = function(elem, id) {
+        let $elem = $(elem);
+        let like = $elem.attr('data-like');
+        if (like) {
+            // The user doesn't want to like this talk anymore
+            _unlikeIcon($elem);
+            _removeLike(id);
+        } else {
+            // The user likes the talk
+            _likeIcon($elem);
+            _addLike(id);
+        }
+    };
 
-	function _likeIcon($elem){
-		let $svg = $elem.find('[data-fa-i2svg]');
-		$svg.attr('data-prefix', 'fas');
-		$elem.attr('data-like', true);
-	}
+    // Initialize Liked Talks
+    window.initLikedTalks = function() {
+        let likes = _getLikes();
+        for (var i = 0; i < likes.length; i++) {
+            _likeIcon(
+                $('.js-talksFilter-like-icon[data-talk=' + likes[i] + ']')
+            );
+        }
+    };
 
-	function _unlikeIcon($elem){
-		let $svg = $elem.find('[data-fa-i2svg]');
-		$svg.attr('data-prefix', 'far');
-		$elem.attr('data-like', null);
-	}
+    function _likeIcon($elem) {
+        $elem.attr('data-like', true);
+    }
 
-	/**
-	 * Add a talk in liked talks
-	 * @param {int} id ID of the talk
-	 */
-	function _addLike(id){
-		let likes = _getLikes();
-		likes.push(id);
-		_setLikes(likes);
+    function _unlikeIcon($elem) {
+        $elem.attr('data-like', null);
+    }
 
-		$.ajax({
-			url: '/like-talk/' + id,
-			type: 'POST',
-			success: function(result) {
-				$('.js-talksFilter-like[data-talk=' + id + '] span').html(result)
-			},
-			error : function(result) {
-			}
-		});
-	}
+    /**
+     * Add a talk in liked talks
+     * @param {int} id ID of the talk
+     */
+    function _addLike(id) {
+        let likes = _getLikes();
+        likes.push(id);
+        _setLikes(likes);
 
-	/**
-	 * Remove a talk from liked talks
-	 * @param {int} id ID of the talk
-	 */
-	function _removeLike(id){
-		let likes = _getLikes();
-		let index = likes.indexOf(id);
-		if (index > -1) {
-			likes.splice(index, 1);
-		}
-		_setLikes(likes);
+        $.ajax({
+            url: '/like-talk/' + id,
+            type: 'POST',
+            success: function(result) {
+                $('.js-talksFilter-like[data-talk=' + id + ']').html(result);
+            },
+            error: function(result) {},
+        });
+    }
 
-		$.ajax({
-			url: '/unlike-talk/' + id,
-			type: 'POST',
-			success: function(result) {
-				$('.js-talksFilter-like[data-talk=' + id + '] span').html(result)
-			},
-			error : function(result) {
-			}
-		});
-	}
+    /**
+     * Remove a talk from liked talks
+     * @param {int} id ID of the talk
+     */
+    function _removeLike(id) {
+        let likes = _getLikes();
+        let index = likes.indexOf(id);
+        if (index > -1) {
+            likes.splice(index, 1);
+        }
+        _setLikes(likes);
 
-	/**
-	 * Return array of ID talks liked by the user.
-	 * (The information is saved in the local storage of the user)
-	 */
-	function _getLikes(){
-		if (window.localStorage) {
-			let likes = JSON.parse(localStorage.getItem('RivieraDEV-2019-Likes'));
-			if(!likes){
-				// Initialize value
-				likes = [];
-				_setLikes(likes);
-			}
-			return likes;
-		}
-		return [];
-	}
+        $.ajax({
+            url: '/unlike-talk/' + id,
+            type: 'POST',
+            success: function(result) {
+                $('.js-talksFilter-like[data-talk=' + id + ']').html(result);
+            },
+            error: function(result) {},
+        });
+    }
 
-	/**
-	 * Save in the local storage an array of ID talks liked by the user
-	 * @param {array} likes 
-	 */
-	function _setLikes(likes){
-		if (window.localStorage) {
-			localStorage.setItem('RivieraDEV-2019-Likes', JSON.stringify(likes));
-		}
-	}
+    /**
+     * Return array of ID talks liked by the user.
+     * (The information is saved in the local storage of the user)
+     */
+    function _getLikes() {
+        if (window.localStorage) {
+            let likes = JSON.parse(
+                localStorage.getItem('RivieraDEV-2019-Likes')
+            );
+            if (!likes) {
+                // Initialize value
+                likes = [];
+                _setLikes(likes);
+            }
+            return likes;
+        }
+        return [];
+    }
 
+    /**
+     * Save in the local storage an array of ID talks liked by the user
+     * @param {array} likes
+     */
+    function _setLikes(likes) {
+        if (window.localStorage) {
+            localStorage.setItem(
+                'RivieraDEV-2019-Likes',
+                JSON.stringify(likes)
+            );
+        }
+    }
 });
