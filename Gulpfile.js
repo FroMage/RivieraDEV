584d5ac9f74caf471a205998eee37bafa04178ed
Jean-Michael <jeanmichael.legait@gmail.com>
2019-04-12 23:19:01 +0200

    Optimize images loading : Serve responsive webP images

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,6 +8,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const merge = require('merge-stream');
 const runSequence = require('run-sequence');
 const wait = require('gulp-wait');
+const responsive = require('gulp-responsive'); // To generate WebP images
 
 sass.compiler = require('node-sass');
 
@@ -40,6 +41,44 @@ gulp.task('sass:watch', function() {
     gulp.watch(STYLE_DIR + '/**/*.scss', () => {
         runSequence('sass', 'autoprefixer');
     });
+});
+
+gulp.task('responsive-images', function() {
+    return gulp
+        .src('public/images/illustrations/*-mini.{png,jpg}')
+        .pipe(
+            responsive(
+                {
+                    // Resize all JPG images to three different sizes: 200, 500, and 630 pixels
+                    '*.jpg': [
+                        {
+                            width: 667,
+                            format: 'webp',
+                            rename: { suffix: '-mobile' },
+                        },
+                        {
+                            width: 768,
+                            format: 'webp',
+                            rename: { suffix: '-tablet' },
+                        },
+                        {
+                            format: 'webp',
+                            rename: { suffix: '-desktop' },
+                        },
+                    ],
+                },
+                {
+                    // Global configuration for all images
+                    // The output quality for JPEG, WebP and TIFF output formats
+                    quality: 70,
+                    // Use progressive (interlace) scan for JPEG and PNG output
+                    progressive: true,
+                    // Strip all metadata
+                    withMetadata: false,
+                }
+            )
+        )
+        .pipe(gulp.dest('public/images/illustrations/responsives/'));
 });
 
 gulp.task('default', ['sass:watch']);
